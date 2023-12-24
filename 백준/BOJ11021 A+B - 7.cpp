94fed5efcfa6c693896ee6161eaa690a9ea33f07
DOYEONG HYUN <doyeong.hyun@gmail.com>
2023-12-24 11:28:30 +0900

    Create BOJ11021 A+B - 7.cpp

---
+++ 백준/BOJ11021 A+B - 7.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int T, A, B;
+	int sum = 0;
+
+	cin >> T;
+	for (int i = 0; i < T; i++) {
+		cin >> A >> B;
+		sum = A + B;
+		cout << "Case #" << i + 1 << ": ";
+		cout << sum << "\n";
+	}
+
+	return 0;
+}
