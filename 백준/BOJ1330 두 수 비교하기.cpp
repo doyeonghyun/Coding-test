5973cd0e235b9efb85367cb33bc2c91c3d05d925
DOYEONG HYUN <doyeong.hyun@gmail.com>
2023-12-24 11:19:52 +0900

    Create BOJ1330 두 수 비교하기.cpp

---
+++ 백준/BOJ1330 두 수 비교하기.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int A, B;
+
+int main() {
+	cin >> A >> B;
+	if (A > B)
+		cout << ">";
+	else if (A < B)
+		cout << "<";
+	else
+		cout << "==";
+	return 0;
+}
